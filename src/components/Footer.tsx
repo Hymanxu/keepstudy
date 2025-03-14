b4ecbcbf8f64e39ac8e499a6081e2d56b0e6cf59
Xu <xu@XudeMacBook-Pro.local>
2025-03-14 18:23:00 +0800

    添加netlify配置

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faTwitter, faWeibo, faWeixin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faWeibo, faWeixin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   return (
